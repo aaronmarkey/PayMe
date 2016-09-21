e231f37b2a1fab035cf01da72e223e786dd36728
Aaron Markey <markeyaaron@gmail.com>
2016-09-21 11:17:26 -0500

    Write calculation function; Write formatting function; Write error alert function;

---
+++ PayMe/Utitlities.swift
@@ -0,0 +1,91 @@
+//
+//  Utitlities.swift
+//  PayMe
+//
+//  Created by Aaron Markey on 9/21/16.
+//  Copyright © 2016 Aaron Markey. All rights reserved.
+//
+
+import UIKit
+
+/**
+ Shows alert message
+ 
+ - Parameter errorMessage: The message to show in the error.
+ - Parameter view: The view to display this alert on top of.
+ */
+func createAndDisplayErrorAlert(errorMessage: String, view: UIViewController) {
+    
+    //create alert conrtroller
+    let alertController = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .Alert)
+    
+    //set cancel button title
+    let cancelButton = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+    
+    //add button to alert controller
+    alertController.addAction(cancelButton)
+    
+    //present the controller
+    view.presentViewController(alertController, animated: true, completion: nil)
+}
+
+
+/**
+ Calculates the overtime rate, overtime hours worked, gross pay, taxes, and net pay
+ 
+ - Parameters:
+    - payRate: The hourly rate of pay.
+    - otRate: The multiplier applied to overtime hours.
+    - hoursWorked: The number of hours worked.
+    - taxRate: The total tax rate that applies to the income
+    - other: Amount to take from pay for any other reason
+ 
+ - Returns: 
+    - otRate: The amount of money earned while working overtime.
+    - otHours: The number of hours worked overtime.
+    - gross: The gross amount of the paycheck.
+    - totalTax: The amount of money taken from the paycheck due to taxes.
+    - net: The amount the user's check will actually be.
+*/
+func calculateResults(payRate: Float, otRate: Float, hoursWorked: Int, taxRate: Float, other: Float) -> (otRate: Float, otHours: Int, gross: Float, totalTax: Float, net: Float) {
+    
+    //calculate overtime hourly rate
+    let otRate = payRate * otRate
+    
+    //calculate number of OT hours worked
+    let otHours: Int;
+    if(hoursWorked > 40 ) {
+        otHours = hoursWorked - 40
+    } else {
+        otHours = 0
+    }
+    
+    //calculate gross pay
+    let gross = 40 * payRate + Float(otHours) * otRate
+    
+    //calculate taxes
+    let totalTax = gross * taxRate
+    
+    //calculate net pay
+    let net = gross - totalTax - other
+    
+    return(otRate, otHours, gross, totalTax, net)
+}
+
+
+/**
+ Formats a number for output on screen
+ 
+ - Parameter number: The number to be formatted.
+ - Parameter style: The style to apply to number.
+ 
+ - Returns: The formatted number as a String.
+*/
+func formatNumber(number: Float, style: NSNumberFormatterStyle) -> String! {
+    let formatter = NSNumberFormatter()
+    formatter.numberStyle = style
+    
+    return formatter.stringFromNumber(number)!
+}
+
+
--- PayMe/ViewController.swift
+++ PayMe/ViewController.swift
@@ -26,16 +26,28 @@ class ViewController: UIViewController {
     
     @IBAction func calculateButton(sender: UIButton) {
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    //MARK: UIResponder functions
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextView) -> Bool {
+        textField.resignFirstResponder();
+        return true
+    }
 }
 
